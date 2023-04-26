bbfaf7e95f53261983a7846957f54b9a226910f3
Kyriakos Andreou <iamIcarus@users.noreply.github.com>
2023-04-26 23:47:57 +0300

    Gas optimisations

--- contracts/SaleAnimalToken.sol
+++ contracts/SaleAnimalToken.sol
@@ -15,6 +15,11 @@ contract SaleAnimalToken{
 
     uint256[] public onSaleAnimalTokenArr;
 
+      // Gas Optimization: Use view instead of constant
+    function isTokenOnSale(uint256 _animalTokenId) public view returns (bool) {
+        return animalTokenPrices[_animalTokenId] > 0;
+    }
+
     //판매등록
     function setForSaleAnimalToken(uint256 _animalTokenId, uint256 _price) public {
         address animalTokenOwner = mintAnimalTokenAddress.ownerOf(_animalTokenId);
@@ -22,7 +27,9 @@ contract SaleAnimalToken{
         //test코드: 맞으면 다음줄 아니면 메시지출력
         require(animalTokenOwner == msg.sender, "Caller is not animal token owner.");
         require(_price > 0, "Price is zero or lower.");
-        require(animalTokenPrices[_animalTokenId] == 0, "This token is already on sale.");//없으면 0 있으면 다른숫자
+
+        // 
+        require(!isTokenOnSale(_animalTokenId), "This token is already on sale.");
         //Mint 컨트렉트에서 판매권한을 줬는가? isApprovedForAll(owner:이컨트렉트만든놈, operator:_mitAnimalTokenAddress)
         require(mintAnimalTokenAddress.isApprovedForAll(animalTokenOwner, address(this)), "Animal token owner did not approve token.");
         //참고로 권한을 주려면 Mint컨트렉트에서 setApprovalForAll(operator:_mitAnimalTokenAddress, approved: true)
@@ -33,11 +40,13 @@ contract SaleAnimalToken{
         onSaleAnimalTokenArr.push(_animalTokenId);
     }
 
+
+
 //payable키워드를 붙여야 metic이 왓다갓다하는 함수를 실행할 수있다.
     function purchaseAnimalToken(uint256 _animalTokenId) public payable{
         uint256 price = animalTokenPrices[_animalTokenId];
         address animalTokenOwner = mintAnimalTokenAddress.ownerOf(_animalTokenId);
-        require(price > 0, "Animal token not sale");
+        require(isTokenOnSale(_animalTokenId), "Animal token not sale");
         require(price <= msg.value, "Caller sent lower than price.");//사려는 owner value에 10 채워넣어야함
         require(animalTokenOwner != msg.sender, "Caller is animal token owner.");//같은owner끼리느 거래불가
     
@@ -45,17 +54,21 @@ contract SaleAnimalToken{
         //이제 주인바꾸기(확인: ownerOf())
         mintAnimalTokenAddress.safeTransferFrom(animalTokenOwner, msg.sender, _animalTokenId);//from, to, tokenid
     
-        //mapping 에서 제거
-        animalTokenPrices[_animalTokenId] = 0;
-        for(uint256 i=0; i<onSaleAnimalTokenArr.length; i++){
-            if(animalTokenPrices[onSaleAnimalTokenArr[i]] == 0){
-                onSaleAnimalTokenArr[i] = onSaleAnimalTokenArr[onSaleAnimalTokenArr.length - 1];//맨뒤로 보낸담에 pop()
-                onSaleAnimalTokenArr.pop();
-            }
+        // Gas Optimization: Use the swap-and-pop technique to remove the purchased token from the onSaleAnimalTokenArr array
+        uint256 lastTokenIndex = onSaleAnimalTokenArr.length - 1;
+        uint256 purchasedTokenIndex = 0;
+        while (onSaleAnimalTokenArr[purchasedTokenIndex] != _animalTokenId) {
+            purchasedTokenIndex++;
         }
+        if (purchasedTokenIndex != lastTokenIndex) {
+            onSaleAnimalTokenArr[purchasedTokenIndex] = onSaleAnimalTokenArr[lastTokenIndex];
+        }
+        onSaleAnimalTokenArr.pop();
+
+        animalTokenPrices[_animalTokenId] = 0;
     }
 
-    function getOnSaleAnimalTokenArrLength() view public returns (uint256){
+    function getOnSaleAnimalTokenArrLength() public view returns (uint256) {
         return onSaleAnimalTokenArr.length;
     }
 }
