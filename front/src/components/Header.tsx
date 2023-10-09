51d477359232396da2eb33af3c2ce80aec7af2b9
LeeEugene1 <uwm1004@gmail.com>
2023-10-09 21:22:41 +0900

    style: nav, modal

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -8,9 +8,11 @@ import { formatAddress } from "@/utils/func";
 import { UseMetaMask } from "@/hooks/UseMetaMask";
 
 const Header:React.FC= () => {
+  const [showModal, setShowModal] = useState(false)
   const [web3, setWeb3] = useState<Web3 | undefined>(undefined)
   const buttonRef = useRef<HTMLButtonElement | null>(null)
   const {wallet, QrUrl, connectKlip, isConnecting, connectMetaMask, disconnect, env} = UseMetaMask()
+  
   useEffect(()=>{
     if(typeof window.ethereum !== 'undefined'){
       try {
@@ -23,23 +25,100 @@ const Header:React.FC= () => {
   },[])
 
   const handleConnetWallet = async () => {
-    if(wallet.accounts.length < 1){
       connectMetaMask()
-    }
-    
+      setShowModal(false)
   }
   const handleConnectKlip = () => {
-    if(wallet.accounts.length < 1){
       connectKlip()
-    }
+      setShowModal(false)
   }
+  const handleConnect = () => {
+    setShowModal(true)
+  }
+
+  const handleCloseModal = (e:any) => {
+    e.preventDefault()
+    setShowModal(false)
+  }
+
   return (
-    <div>
-        <div className="navbar">
-          <Link href='/'>HOME</Link>
-          <Link href='/list'>SHOP</Link>
-          <Link href='/mint'>MINT</Link>
+    <div id="app">
+      <header id="navbar" className=''>
+        <nav className="border-b border-slate-900/10 py-4 flex justify-between items-center w-[92%] mx-auto">
+          <div className="">
+            <Link href='/'>HOME</Link>
+          </div>
+          <div className="">
+            <ul className="flex items-center gap-[4vw]">
+              <Link href='/list' className="hover:text-gray-500">MINE</Link>
+              <Link href='/mint' className="hover:text-gray-500">CREATE</Link>
+            </ul>
+          </div>
+          <div>
+            {
+              (wallet.accounts.length < 1) ? 
+              <button className="px-4 py-2 font-semibold text-sm bg-white text-slate-700 border border-slate-300 rounded-md shadow-sm ring-gray-border-300 ring-offset-2 ring-offset-slate-50 
+              focus:outline-none focus:ring-2 
+              dark:bg-slate-700 dark:text-slate-200 dark:border-transparent"
+                onClick={handleConnect}
+              >
+                Connect Wallet
+              </button>
+              :
+              <button className="px-4 py-2 font-semibold text-sm bg-white text-slate-700 border border-slate-300 rounded-md shadow-sm ring-gray-border-300 ring-offset-2 ring-offset-slate-50 
+              focus:outline-none focus:ring-2 
+              dark:bg-slate-700 dark:text-slate-200 dark:border-transparent"
+                onClick={disconnect}
+              >
+                Disconnect</button>
+            }
+          
+            
+          </div>
+
+        </nav>
+      </header>
+      {
+        showModal &&
+        <div id="modal">
+          <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm
+        flex justify-center items-center" onClick={handleCloseModal}>
+          </div>
+          <div className="shell relative overflow-hidden rounded-none md:rounded-3xl">
+            <div className="flex justify-between text-center">
+              <h3>Connect wallet</h3>
+              <svg onClick={handleCloseModal} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </div>
+            <div className="modal-body">
+                <div className="m-4 space-y-2">
+                  <div className="block" onClick={handleConnetWallet}>
+                      <button type="button" className="button px-[22px] flex w-full items-center justify-center" data-testid="button-connnect-wallet-injected">
+                        <img src="https://snapshot.4everland.link/ipfs/QmTE7VPXMhriKAobMWEiC5S3oG22p4G6AXGyGdNWQTQ3Fv" height="28" width="28" className="-mt-1 mr-2" alt="MetaMask"/> 
+                        MetaMask
+                      </button>
+                  </div>
+                  {/* <div className="block">
+                        <button type="button" className="button px-[22px] flex w-full items-center justify-center gap-2">
+                          <img src="https://snapshot.4everland.link/ipfs/QmZRVqHpgRemw13aoovP2EaQdVtjzXRaQGQZsCLXWaNn9x" height="25" width="25" alt="WalletConnect"/>
+                          <span>WalletConnect</span>
+                        </button>
+                  </div> */}
+                    <div className="block">
+                        <button type="button" className="button px-[22px] flex w-full items-center justify-center gap-2">
+                            <div className="flex justify-center w-[28px]">
+                              <img src="/logo_klip.png" className="max-w-[50px]" width="50" alt="Klip"/>
+                            </div>
+                            <span>Klip</span>
+                        </button>
+                  </div>
+                </div>
+            </div>
+          </div>
         </div>
+      }
+            
         {
         (wallet.accounts.length < 1) &&
         <>
@@ -62,7 +141,6 @@ const Header:React.FC= () => {
         wallet.accounts.length > 0 &&
         <div>
           <p>{formatAddress(wallet.accounts[0])}</p>
-          <button onClick={disconnect}>Logout</button>
         </div>
       }
     </div>
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,18 +1,54 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
+  darkMode:'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      ...colors,
+      stone: colors.warmGray,
+      sky: colors.lightBlue,
+      neutral: colors.trueGray,
+      gray: colors.coolGray,
+      slate: colors.blueGray,
+      // 'white': '#ffffff',
+      // 'blue': '#1fb6ff',
+      // 'purple': '#7e5bef',
+      // 'pink': '#ff49db',
+      // 'orange': '#ff7849',
+      // 'green': '#13ce66',
+
+      // 'black':'1111111',
+      // 'yellow': '#f7e93e',
+      // 'yellow-dark':'#f7d300',
+      // 'navy':'#391bff',
+      // 'navy-dark':'#0008f3',
+      // 'gray-border':'#e0e0e0',
+      // 'gray-link':'#444444',
+
+      // 'gray-dark': '#273444',
+      // 'gray': '#8492a6',
+      // 'gray-light': '#d3dce6',
     },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
-  plugins: [],
+  plugins: [
+  ],
 }
